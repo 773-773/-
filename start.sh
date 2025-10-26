27f0e5a41c20ac226a351fc2f941a9689f1378b2
773-773 <na77sevensea@gmail.com>
2025-10-26 20:51:26 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 cd /app
+
+# pb_data が無ければ作成
+mkdir -p /app/pb_data
 chmod -R 777 /app/pb_data
 
-# PocketBase を静的ファイルの公開ディレクトリとして pb_public をセット
+# pb_public の存在確認
+if [ -d "/app/pb_public" ]; then
+  echo "✅ pb_public directory found."
+else
+  echo "❌ pb_public directory NOT found! exiting..."
+  ls -la /app   # デバッグ用にフォルダ内容表示
+  exit 1
+fi
+
+# PocketBase 起動
 ./pocketbase serve --http="0.0.0.0:${PORT}" --publicDir="pb_public"
