6c35538bf1a4418c36b9aa7b3f73fca3e557b67e
773-773 <na77sevensea@gmail.com>
2025-10-26 20:20:35 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
-#!/bin/sh
-set -e
-
-# PocketBaseのデータ保存先フォルダを作成（既にある場合もOK）
-mkdir -p /app/pb_data
-# マウントされたディスクに書き込めるように権限付与
+#!/bin/bash
+cd /app
 chmod -R 777 /app/pb_data
 
-echo "✅ Starting PocketBase with data directory: /app/pb_data"
-ls -la /app/pb_data
-
-# PocketBase を起動
-exec ./pocketbase serve --dir /app/pb_data --http 0.0.0.0:${PORT}
+# PocketBase を静的ファイルの公開ディレクトリとして pb_public をセット
+./pocketbase serve --http="0.0.0.0:${PORT}" --publicDir="pb_public"
