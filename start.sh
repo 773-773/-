11fd34bb46ef5570a44aa790c6a7d2e0e1c7dc44
773-773 <na77sevensea@gmail.com>
2025-10-31 22:26:10 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /app
 
-# ✅ pb_data が Render で自動マウントされるので自分では作らない
+# pb_data が無ければ作成
+mkdir -p /app/pb_data
+chmod -R 777 /app/pb_data
 
-# ✅ PocketBase起動
-./pocketbase serve --http="0.0.0.0:${PORT}" --dir="/app/pb_data" --publicDir="/app/pb_public"
+# ✅ 公開ディレクトリ指定を削除
+./pocketbase serve --http="0.0.0.0:${PORT}" --dir="/app/pb_data"
