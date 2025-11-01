cb89778e07aca6bbe3dc6297d840c84a95dd8ce7
773-773 <na77sevensea@gmail.com>
2025-11-01 22:42:21 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ chmod -R 777 /app/pb_data
 
 PORT=${PORT:-8080}
 
-# ✅ baseUrlが使える最新版PocketBase向け
+# ✅ baseUrl がサポートされる最新版用
 ./pocketbase serve --http="0.0.0.0:${PORT}" --dir="/app/pb_data" --baseUrl="/pb"
